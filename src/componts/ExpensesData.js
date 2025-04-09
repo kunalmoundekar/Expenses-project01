86a209b0e812b82598939d8388becc033f63a00b
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-04-09 16:29:09 +0530

    add one more card

--- src/componts/ExpensesData.js
+++ src/componts/ExpensesData.js
@@ -3,6 +3,7 @@ const ExpensesData = [
     {  title: 'New TV', amount: 799.49, date: new Date(2023, 2, 12) },
     {  title: 'Car Insurance', amount: 294.67, date: new Date(2023, 2, 28) },
     {  title: 'New Desk (Wooden)', amount: 450, date: new Date(2023, 5, 12) },
+    {  title: 'New Desk (Wooden)', amount: 450, date: new Date(2023, 5, 12) }
   ];
   
   export default ExpensesData;
