2f562026a6dd11639abc1d6c404a2716e4cf6047
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-04-11 15:08:00 +0530

    remove unnecessary comment

--- src/componts/ExpenseItem.js
+++ src/componts/ExpenseItem.js
@@ -3,23 +3,12 @@
 import './Expenseitem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
-// import React, { useState } from 'react';
+
 
 
 
 
 const ExpenseItem = (props) => {
-
-  // const [title, setTitle] = useState("")
-  // const [newtitle, setnewTitle] = useState("")
-
-  // const clickHadler = () => {
-  //   setTitle(newtitle)
-  // }
-
-  // const changeHandler = (event) => {
-  //   setnewTitle(event.target.value)
-  // }
 
 
   return (
@@ -29,8 +18,6 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      {/* <input type='text' value={newtitle} onChange={changeHandler}></input>
-      <button onClick={clickHadler}>change title</button> */}
     </Card>
   );
 };
